ef7cba2dab12d7387d1d2e15817584fea3ea99a3
Seth S. Henshaw <seth.henshaw@gmail.com>
2021-11-06 08:42:56 -0400

    Adding basic Streamlit app

---
+++ src/data_view_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Biblical Learning Toolkit (bibLearn) - Data Exploration Tool')
+st.write("The following is a dashboard for interacting with basic statistics of the Bible.")
