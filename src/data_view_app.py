a962126e2630a56a98d13aee50e1fca81dc6c9e4
Seth S. Henshaw <seth.henshaw@gmail.com>
2021-11-06 10:58:16 -0400

    basic plumbing for streamlit app

--- src/data_view_app.py
+++ src/data_view_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from biblearn.sword import loadData
 
+data = loadData('akjv')
 st.title('Biblical Learning Toolkit (bibLearn) - Data Exploration Tool')
 st.write("The following is a dashboard for interacting with basic statistics of the Bible.")
+st.write("John 3:16:  {}".format(data))
