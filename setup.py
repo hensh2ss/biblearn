549a7fb57ad5d8ed9dc854a296017507097ccd4f
Seth S. Henshaw <seth.henshaw@gmail.com>
2021-11-06 11:16:36 -0400

    updating fixVersion for twine upload

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="biblearn",
-    version="0.0.1",
+    version="0.0.2",
     author="Seth S. Henshaw",
     author_email="seth.henshaw@gmail.com",
     description="Biblical Learning Toolkit (biblearn)",
