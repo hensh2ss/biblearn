5321f5e6d430233848926f6a340cea669080ef5e
codykrug <codykrug@knights.ucf.edu>
2021-11-07 23:29:51 -0500

    getting started with cminson word similarity

--- WebResources.md
+++ WebResources.md
@@ -11,3 +11,26 @@ http://www.crosswire.org/sword/modules/ModDisp.jsp?modType=Bibles
 # Various SWORD Bible Versions
 https://tgc-dk.gitlab.io/pysword/examples.html
 http://www.crosswire.org/sword/modules/ModDisp.jsp?modType=Bibles
+
+
+# Christopher Minson
+
+Let's recreate this [blog](https://www.christopherminson.com/articles/aibible.html) from this [repo](https://github.com/cminson/embeddedbible)
+
+## Build the model
+
+### Quick corrections to get the model
+
+before you run textinput,py, load this
+
+```
+import nltk
+nltk.download('punkt')
+```
+
+change the size parameter in ```gensim.models.Word2Vec``` from ```size``` to ```vector_size```
+
+fixed typos in lines 56 and 58 by adding wv from model.mostsimilar to model.wv.mostsimilar
+
+
+
--- src/data_view_app.py
+++ src/data_view_app.py
@@ -1,9 +1,32 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from biblearn.sword import loadData
+
+import gensim
 
 data = loadData('akjv')
 st.title('Biblical Learning Toolkit (bibLearn) - Data Exploration Tool')
 st.write("The following is a dashboard for interacting with basic statistics of the Bible.")
 st.write("John 3:16:  {}".format(data))
+
+#c. minson's model
+
+model = gensim.models.Word2Vec.load("./MODELS/model.words.10")
+
+KEYS = model.wv.index_to_key
+
+CHOSEN = st.multiselect("Enter words below", KEYS)
+
+send = st.button("Find similar words")
+
+if send:
+    result = model.wv.most_similar(positive=CHOSEN, topn=10)
+    print(result)
+    fig, ax = plt.subplots()
+    x_val = [x[0] for x in result]
+    y_val = [x[1] for x in result]
+    ax.plot(x_val,y_val)
+    plt.xticks(rotation = 45)
+    st.pyplot(fig)
